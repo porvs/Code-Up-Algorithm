0ac817b0e72e97d3bba9b33a77923c73758b585d
Hayeong Jeong <hayoung00045@gmail.com>
2021-12-20 16:16:27 +0900

    python-extension

---
+++ 6048 : [기초-비교연산] 정수 2개 입력받아 비교하기1.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+if(a < b):
+    print("True")
+
+
+elif(a >= b):
+    print("False")
---
+++ 6049 : [기초-비교연산] 정수 2개 입력받아 비교하기2.py
@@ -0,0 +1,7 @@
+a, b = input().split()
+
+if(a == b):
+    print("True")
+
+else:
+    print("False")
