7fa8ea7a297f293e658e8493f9305ef80d4ae282
Hayeong Jeong <hayoung00045@gmail.com>
2022-01-06 15:53:11 +0900

    Python extension

---
+++ 6075  [기초-반복실행구조] 정수 1개 입력받아 그 수까지 출력하기1.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = 0
+while b <= a:
+    print(b)
+    b += 1
+
+
+
+
---
+++ 6080  [기초-종합] 주사위 2개 던지기.py
@@ -0,0 +1,4 @@
+n,m = int(input().split())
+for i in range(1, n+1) :
+    for j in range(1, m+1) :
+        print(i, j)
